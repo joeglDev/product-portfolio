a3fbe09864cb2a3079830fae7a72ce3eb9ea5d29
hiroji <joegilbertdev@outlook.com>
2023-08-20 18:53:59 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LandingPage } from './pages/LandingPage';
+
+function App() {
+
+  return (
+    //nav bar here
+
+    //defaut splash page here
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage></LandingPage>}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
